4b0bb1f793518166931bb7474bf75c212c41fdba
Amanjit313 <amanjitsingh313@gmail.com>
2022-06-27 16:23:08 +0200

    table

---
+++ database/migrations/2022_06_27_130855_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 200);
+            $table->string('stazione_di_arrivo', 200);
+            $table->time('orario_di_partenza', 0)->unsigned();
+            $table->time('orario_di_arrivo', 0)->unsigned();
+            $table->mediumInteger('codice_treno')->unsigned();
+            $table->smallInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('is_cancellato')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
